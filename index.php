8fdacb02b8502a4df61d3ea25a064f6a39ab144b
Nathan Badoit <nathanbadoithb@gmail.com>
2022-07-13 09:45:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO List</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus facere quidem itaque nulla voluptates dicta, a fugiat ut, blanditiis cum ipsum amet consectetur dolorem ullam voluptatem error provident necessitatibus quam!</div>
+        </li>
+        <li>
+            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, quasi tempore. Repellat aliquam facilis ratione nemo asperiores, quod repellendus illo obcaecati quis libero necessitatibus quam vel provident mollitia voluptates alias?</div>
+        </li>
+        <li>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam aperiam molestias recusandae assumenda, cum earum laboriosam omnis cupiditate, libero maxime consectetur quas quod officia ipsam obcaecati quibusdam minima pariatur.</div>
+        </li>
+        <li>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, nostrum? Consequatur nihil vel laborum, perspiciatis deserunt voluptatum fugit vero ut obcaecati. Ipsa exercitationem culpa voluptatum neque veniam aspernatur cupiditate incidunt!</div>
+        </li>
+    </ul>
+    
+</body>
+</html>
